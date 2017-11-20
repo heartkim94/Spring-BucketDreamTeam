9ea7f7120a00d50e09c8503ef7ecac50445d814c
User <User@User-PC>
2017-11-20 15:29:58 +0900

    test

--- teamProject/src/main/java/com/pknu/teamproject/HomeController.java
+++ teamProject/src/main/java/com/pknu/teamproject/HomeController.java
@@ -22,22 +22,27 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
+//	@RequestMapping(value = "/", method = RequestMethod.GET)
+//	public String home(Locale locale, Model model) {
+//		logger.info("Welcome home! The client locale is {}.", locale);
+//		
+//		Date date = new Date();
+//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+//		
+//		String formattedDate = dateFormat.format(date);
+//		
+//		model.addAttribute("serverTime", formattedDate );
+//		
+//		return "home";
+//	}
 	
 	@RequestMapping(value = "/test.do")
 	public String test() {
+		return "test";
+	}
+	
+	@RequestMapping(value = "/teamproject/test.do")
+	public String test1() {
 		return "test";
 	}
 	
