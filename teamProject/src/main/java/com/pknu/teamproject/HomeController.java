ffae51f944301ccc0859920cf9d5cccff385c55b
whddn <whddn29@gmail.com>
2017-11-21 14:29:53 +0900

    aa

--- teamProject/src/main/java/com/pknu/teamproject/HomeController.java
+++ teamProject/src/main/java/com/pknu/teamproject/HomeController.java
@@ -26,7 +26,6 @@ public class HomeController {
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		logger.info("test");
-		logger.info("one more");
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
