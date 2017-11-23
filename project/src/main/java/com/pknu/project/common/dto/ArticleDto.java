23c2cbeef4c7236837332f7feab5bc17fcbecae3
User <User@User-PC>
2017-11-23 12:55:17 +0900

    sql 변경

---
+++ project/src/main/java/com/pknu/project/common/dto/ArticleDto.java
@@ -0,0 +1,75 @@
+package com.pknu.project.common.dto;
+
+public class ArticleDto {
+	private int articleNum;
+	private String id;
+	private String title;
+	private String content;
+	private int depth;
+	private int hit;
+	private int groupId;
+	private int pos;
+	private String writeDate;
+	private int fileStatus;
+	
+	public int getArticleNum() {
+		return articleNum;
+	}
+	public void setArticleNum(int articleNum) {
+		this.articleNum = articleNum;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getDepth() {
+		return depth;
+	}
+	public void setDepth(int depth) {
+		this.depth = depth;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	public int getGroupId() {
+		return groupId;
+	}
+	public void setGroupId(int groupId) {
+		this.groupId = groupId;
+	}
+	public int getPos() {
+		return pos;
+	}
+	public void setPos(int pos) {
+		this.pos = pos;
+	}
+	public String getWriteDate() {
+		return writeDate;
+	}
+	public void setWriteDate(String writeDate) {
+		this.writeDate = writeDate;
+	}
+	public int getFileStatus() {
+		return fileStatus;
+	}
+	public void setFileStatus(int fileStatus) {
+		this.fileStatus = fileStatus;
+	}
+}
