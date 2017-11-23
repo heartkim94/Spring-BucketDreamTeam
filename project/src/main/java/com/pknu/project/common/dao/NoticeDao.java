f002eaa0e7daa1e85bd469cdd6c820ca41c94139
User <User@User-PC>
2017-11-23 12:55:28 +0900

    a

---
+++ project/src/main/java/com/pknu/project/common/dao/NoticeDao.java
@@ -0,0 +1,11 @@
+package com.pknu.project.common.dao;
+
+import java.util.HashMap;
+import java.util.List;
+
+import com.pknu.project.common.dto.ArticleDto;
+
+public interface NoticeDao {
+	List<ArticleDto> list(HashMap<String, String> paramMap);
+	ArticleDto get
+}
