e356397153a7e99e997916dee43cb097fa92e7a2
User <User@User-PC>
2017-12-17 16:18:24 +0900

    ...

--- project/src/main/java/com/pknu/project/group/controller/GroupController.java
+++ project/src/main/java/com/pknu/project/group/controller/GroupController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.pknu.project.common.dto.ArticleDto;
 import com.pknu.project.common.dto.BoardDto;
